37f45ce4332da780d6613d00bd71b3c79cb18049
JABERI Mohamed <mohamed-habib.jaberi@bred.fr>
2026-09-26 13:41:22 +0200

    docs: explain module and function responsibilities

--- src/stores/vectordb/VectorDBEnums.py
+++ src/stores/vectordb/VectorDBEnums.py
@@ -1,14 +1,19 @@
+"""Define vector-database abstractions, enums, and provider construction logic."""
+
 from enum import Enum
 
 class VectorDBEnums(Enum):
+    """Encapsulate the responsibilities and state of the VectorDBEnums component."""
     QDRANT = "QDRANT"
     PGVECTOR = "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
+    """Encapsulate the responsibilities and state of the DistanceMethodEnums component."""
     COSINE = "cosine"
     DOT = "dot"
 
 class PgVectorTableSchemeEnums(Enum):
+    """Encapsulate the responsibilities and state of the PgVectorTableSchemeEnums component."""
     ID = 'id'
     TEXT = 'text'
     VECTOR = 'vector'
@@ -17,9 +22,11 @@ class PgVectorTableSchemeEnums(Enum):
     _PREFIX = 'pgvector'
 
 class PgVectorDistanceMethodEnums(Enum):
+    """Encapsulate the responsibilities and state of the PgVectorDistanceMethodEnums component."""
     COSINE = "vector_cosine_ops"
     DOT = "vector_l2_ops"
 
 class PgVectorIndexTypeEnums(Enum):
+    """Encapsulate the responsibilities and state of the PgVectorIndexTypeEnums component."""
     HNSW = "hnsw"
     IVFFLAT = "ivfflat"
